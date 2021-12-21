8ac71bda8d59caaa1902559077107d583dadccf5
Şeyma Tezcan <71821411+seyma-png@users.noreply.github.com>
2021-12-22 01:32:33 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,28 +1,20 @@
 package com.company;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        double kdv=0.18;
         Scanner input=new Scanner(System.in);
-        System.out.println("Matematik notunuz: ");
-        int matNot=input.nextInt();
-        System.out.println("Fizik notunuz: ");
-        int fizikNot=input.nextInt();
-        System.out.println("Kimya notunuz: ");
-        int kimyaNot=input.nextInt();
-        System.out.println("Türkçe notunuz: ");
-        int turkceNot=input.nextInt();
-        System.out.println("Tarih notunuz: ");
-        int tarihNot=input.nextInt();
-        System.out.println("Müzik notunuz: ");
-        int muzikNot=input.nextInt();
-
-        int toplamPuan=(matNot+fizikNot+kimyaNot+turkceNot+tarihNot+muzikNot)/6;
-        System.out.println("Ortalamanız: "+toplamPuan);
-
-
+        System.out.println("Tutarı giriniz: ");
+        double tutar=input.nextDouble();
+        double kdvTutar=(tutar*18)/100;
+        double kdvliTutar=tutar+tutar*kdv;
+        System.out.println("KDV' siz fiyat: "+tutar);
+        System.out.println("KDV' li fiyat: "+kdvliTutar);
+        System.out.println("KDV Tutarı: "+kdvTutar);
 
 
     }
